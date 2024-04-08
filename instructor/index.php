34450861e97e07b5dda5bdb64d14fd92952bf6ea
FullStackDev. Eugene <103896624+businessweb01@users.noreply.github.com>
2024-04-08 19:04:51 +0800

    Add files via upload

---
+++ instructor/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <input type="checkbox" class="checkbox" id="checkbox">
+    <label for="checkbox" class="checkbox-label">
+        <i class='bx bxs-moon' ></i>
+        <i class='bx bxs-sun' ></i>
+        <span class="ball"></span>
+    </label>
+</body>
+</html>
